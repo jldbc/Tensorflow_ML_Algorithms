1da1d5ca7cf675c091dc259549bc2d6611508800
James LeDoux <ledouxja@bc.edu>
2016-12-27 19:17:51 -0800

    generalize knn to work for any value of k

--- knn.py
+++ knn.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 import numpy as np
+from scipy import stats
 from tensorflow.examples.tutorials.mnist import input_data
 
-#let's use MNIST for this
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 X_train, y_train = mnist.train.next_batch(5000)
 X_test, y_test = mnist.test.next_batch(100)
+
+k = 3
 
 target_x = tf.placeholder("float", [1,784]) #target vector
 X = tf.placeholder("float", [None, 784]) #matrix of observations to compare to target
@@ -16,24 +18,24 @@ l1_dist= tf.reduce_sum(tf.abs(tf.sub(X, target_x)), 1)  #euclidean distance. the
 
 l2_dist = tf.reduce_sum(tf.square(tf.sub(X, target_x)), 1)  #euclidean distance. the sum of squared differences between elements, row-wise.
 
-"""
-next: make this a loop to allow k>1.
-arg min distance. append class to list. slice that observation 
-from X and Y. Repeat K times to get the KNN. 
-""" 
-nn = tf.argmin(l1_dist, 0)
+#nn = tf.argmin(l1_dist, 0)
+nn = tf.nn.top_k(-l1_dist, k)
 
 init = tf.initialize_all_variables()
- 
 accuracy_history = []
 with tf.Session() as sess:
 	sess.run(init)
 	for obs in range(X_test.shape[0]):
 		nn_index = sess.run(nn, feed_dict = {X: X_train, y: y_train, target_x: np.asmatrix(X_test[obs])})
-		nn_class = np.argmax(y_train[nn_index])
+		pred_classes = []
+		for i in range(k):
+			nn_class = np.argmax(y_train[nn_index[1][i]])
+			#print nn_class
+			pred_classes.append(nn_class)
+		predicted_class = stats.mode(pred_classes)[0][0]
 		true_class = np.argmax(y_test[obs])
-		print "True class: " + str(true_class) + ", predicted class: " + str(nn_class)
-		if nn_class == true_class:
+		print "True class: " + str(true_class) + ", predicted class: " + str(predicted_class)
+		if predicted_class == true_class:
 			accuracy_history.append(1)
 		else:
 			accuracy_history.append(0)
